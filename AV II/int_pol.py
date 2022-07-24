0c9caecc172d56a82c54738e689ac347e820a6e3
Amphy <gleaodecastro@gmail.com>
2022-07-24 15:32:58 -0300

    quero morrer

--- AV II/int_pol.py
+++ AV II/int_pol.py
@@ -3,9 +3,6 @@ from sl import eliminacao_gaussiana
 import time
 start_time = time.time()
 
-# e^(-1/x^2) de 1 a 2:
-x = [1.0 ,1.5 ,2.0]
-y = [0.36787944117144233, 0.641180388429954582251520479131204482604147,  0.7788007830714]
 
 def int_pol(x, y):
     q = len(x)
@@ -22,5 +19,7 @@ def pol_coef(x, y):
     P = eliminacao_gaussiana(int_pol(x, y))
     for i in range(len(P)):
         print(P[i],"X ^",i)
-pol_coef(x, y)
-print("\n======>",time.time()-start_time,"<====== segundos")
+   
+         
+
+
